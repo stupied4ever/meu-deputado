975e62386b0e17c0216348f463afcaef0ee015aa
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-04-19 23:54:51 -0300

    json de deputados

--- app/models/deputado.rb
+++ app/models/deputado.rb
@@ -1,25 +1,30 @@
 class Deputado < ActiveRecord::Base
-	attr_accessible :anexo, :email, :fone, :gabinete, :id_parlamentar, :ide_cadastro, :matricula, :nome, :nome_parlamentar, :partido, :sexo, :uf
+	attr_accessible :anexo, :email, :fone, :gabinete, :id_parlamentar, 
+		:ide_cadastro, :matricula, :nome, :nome_parlamentar, :partido, 
+		:sexo, :uf
 
-	validates_presence_of :email, :fone, :id_parlamentar, :ide_cadastro, :matricula, :nome, :nome_parlamentar, :partido, :sexo, :uf
+	validates_presence_of :email, :fone, :id_parlamentar, :ide_cadastro, 
+		:matricula, :nome, :nome_parlamentar, :partido, :sexo, :uf
 
-	SOAP_FIELDS = :anexo, :email, :fone, :gabinete, :id_parlamentar, :ide_cadastro, :matricula, :nome, :nome_parlamentar, :partido, :sexo, :uf
+	SOAP_FIELDS = :anexo, :email, :fone, :gabinete, :id_parlamentar, 
+		:ide_cadastro, :matricula, :nome, :nome_parlamentar, :partido, :sexo, :uf
 
 	has_many :deputado_comissoes
 	has_many :deputado_comissoes_titular,  
-			 :class_name => "DeputadoComissao", :source => :deputado_comissao,
-			 :conditions => ['deputado_comissoes.cadeira = ?', "titular"]
+
+	:class_name => "DeputadoComissao", :source => :deputado_comissao,
+	:conditions => ['deputado_comissoes.cadeira = ?', "titular"]
 
 	has_many :deputado_comissoes_suplente,  
-			 :class_name => "DeputadoComissao", :source => :deputado_comissao,
-			 :conditions => ['deputado_comissoes.cadeira = ?', "suplente"]
+		:class_name => "DeputadoComissao", :source => :deputado_comissao,
+		:conditions => ['deputado_comissoes.cadeira = ?', "suplente"]
 
 	has_many :comissoes, :through => :deputado_comissoes
 
 	has_many :comissoes_titular, :through => :deputado_comissoes_titular,
-			 :class_name => "Comissao", :source => :comissao
+		:class_name => "Comissao", :source => :comissao
 
 	has_many :comissoes_suplente, :through => :deputado_comissoes_suplente,
-			 :class_name => "Comissao", :source => :comissao
+		:class_name => "Comissao", :source => :comissao
 
 end
